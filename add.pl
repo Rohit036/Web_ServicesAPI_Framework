a8ad9e0a95e625e3fd623c174a4f559a3894baf4
Anmol Porwal <anmolporwal@ymail.com>
2013-12-09 20:52:51 +0530

    Update add.pl

--- add.pl
+++ add.pl
@@ -1,35 +1,24 @@
-$num = shift @ARGV;
-$num1 = shift @ARGV;
-
-if($input_method == add){
-sub add;
-}
-else if 
-
-
-
-
 sub add{
 my $num  = $_[0];
 my $num1 = $_[1];
 if($input_method == add)
-print $num + $num1;
+return $num + $num1;
 }
 
 sub subtract{
 my $num  = $_[0];
 my $num1 = $_[1];
-print $num - $num1;
+return $num - $num1;
 }
 
 sub multiply{
 my $num  = $_[0];
 my $num1 = $_[1];
-print $num * $num1;
+return $num * $num1;
 }
 
 sub divide{
 my $num  = $_[0];
 my $num1 = $_[1];
-print $num / $num1;
+return $num / $num1;
 }
