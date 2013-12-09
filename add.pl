1b39bffe2e9d6188082175eed3afe10a8f28d522
Rohit Kumar Singh <Rohit.Singh@st.niituniversity.in>
2013-12-10 00:57:26 +0530

    Update add.pl
    Missed the last line 1; which affected the require clause in other files which gave error as the add.pl is not returning any true value.

--- add.pl
+++ add.pl
@@ -1,24 +1,29 @@
 sub add{
 my $num  = $_[0];
 my $num1 = $_[1];
-if($input_method == add)
+if($input_method == add){
 return $num + $num1;
-}
+}}
 
 sub subtract{
 my $num  = $_[0];
 my $num1 = $_[1];
+if($input_method == add){
 return $num - $num1;
-}
+}}
 
 sub multiply{
 my $num  = $_[0];
 my $num1 = $_[1];
+if($input_method == add){
 return $num * $num1;
-}
+}}
 
 sub divide{
 my $num  = $_[0];
 my $num1 = $_[1];
+if($input_method == add){
 return $num / $num1;
 }
+}
+1;
