75643cf13bdcc7cc0a0cdca456ccded49074257c
Anmol Porwal <anmolporwal@ymail.com>
2013-12-14 18:31:05 +0530

    Update Client_Script.pl

--- Client_Script.pl
+++ Client_Script.pl
@@ -1,48 +1,50 @@
 #!/usr/bin/perl
-sub remote_proc
-{
-	use strict;
-	use warnings;
-	use IO::Socket::INET;
+use strict;
+use warnings;
+use IO::Socket::INET;
 
-	my $method = $_[0];
-	my $input = $_[1];
-	my $input1 = $_[2];
-	my $ans;
-	my $socket;
-	my $serverdata;
-	my $clientdata;
+my $socket;
+my $serverdata;
 
-	$socket = new IO::Socket::INET (
-		PeerHost => '127.0.0.1',
-		PeerPort => '0155',
-		Proto => 'tcp',
-		) or die "$!\n";
-
-	print "Connected to the Server.\n";
+$socket = new IO::Socket::INET (
+  #PeerHost => '172.19.6.195',
+  PeerHost => '127.0.0.1',
+  PeerPort => '0155',
+  Proto => 'tcp',
+) or die "$!\n";
 
 # read the message sent by server.
 $serverdata = <$socket>;
-#print "Message from Server : $serverdata";
+print "Message from Server : $serverdata";
+
+print "Connected to the Server.\n";
+
+sub remote_proc
+{
+my $method = $_[0];
+my $input = $_[1];
+my $input1 = $_[2];
+my $ans;
+my $clientdata;
 
 $clientdata = $method." ".$input." ".$input1;
 my $i = 0;
 while($i < 2)
 {
-	if(defined($serverdata = <$socket>)) # To handle the error saying something abrupt with HANDLE something something.
-	{
-		$ans = $serverdata;
-	}
-	if($clientdata eq "quit")
-	{
-		$socket->close();
-		last;
-	}
-	else
-	{
-		print $socket "$clientdata\n";
-	}
-	$i++;
+if(defined($serverdata = <$socket>))
+{
+$ans = $serverdata;
+}
+if($clientdata eq "quit")
+{
+$socket->close();
+last;
+}
+else
+{
+print $socket "$clientdata\n";
+}
+$i++;
 }
 return $ans;
 }
